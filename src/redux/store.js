24cae25c564770cef24f783c5fc6aa9d6b986588
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-01-24 21:44:49 +0200

    whitelist

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filterSlice';
 
 const persistConfig = {
-  key: 'contacts',
+  key: 'root',
   storage,
   whitelist: ['contacts'],
 };
