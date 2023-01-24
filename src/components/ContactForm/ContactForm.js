c908e257d1c04ae2fe52359910e40e3b027d7441
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-01-24 18:47:05 +0200

    deploy!

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -47,7 +47,7 @@ const ContactForm = () => {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            placeholder="+3807777777"
+            placeholder="+380777777"
           />
         </Label>
         <AddButton type="submit">Add Contact</AddButton>
