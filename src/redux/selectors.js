b2bdcf0c705a14d9e2e66506f0d960774562a009
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-01-28 15:34:08 +0200

    memoisation

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectIsLoading = state => state.contacts.isLoading;
@@ -6,10 +8,8 @@ export const selectError = state => state.contacts.error;
 
 export const selectInputFilter = state => state.filter;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectInputFilter(state);
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
-};
+export const selectVisibleContacts = createSelector(
+  [selectInputFilter, selectContacts],
+  (filter, contacts) =>
+    contacts.filter(({ name }) => name.toLowerCase().includes(filter))
+);
