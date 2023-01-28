38e155cc1d014ced10bbcd6a38c955e188ac427a
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-01-28 15:38:36 +0200

    isLoading and error added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import { AppHeading } from './App.styled';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
 import { useEffect } from 'react';
 import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -18,7 +20,7 @@ export const App = () => {
     <>
       <AppHeading>PhoneBook</AppHeading>
       <ContactForm />
-      {selectIsLoading && !selectError && <b>Loading contacts...</b>}
+      {isLoading && !error && <p>Loading...</p>}
       <ContactList />
     </>
   );
